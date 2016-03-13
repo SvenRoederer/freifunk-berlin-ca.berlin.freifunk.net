1b06952904f42b41c1562105c4be20dd0809af46
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-03-13 02:20:14 +0100

    config.py: remove leftovers from initial try to use python-dicts

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ CACERT_FILE = '/tmp/ffca.crt'
 CAKEY_FILE = '/tmp/ffca.key'
 NEWKEY_ALG = 'rsa'
 NEWKEY_SIZE = 1024
-NEWCERT_COUNTRY = "DE",
-NEWCERT_STATE = "Eastern Germany",
-NEWCERT_LOCATION = "Berlin",
-NEWCERT_ORGANIZATION = "Foerderverein Freie Netzwerke e.V.",
-NEWCERT_DURATION = 10*365*24*60*60, # 10 years
+NEWCERT_COUNTRY = "DE"
+NEWCERT_STATE = "Eastern Germany"
+NEWCERT_LOCATION = "Berlin"
+NEWCERT_ORGANIZATION = "Foerderverein Freie Netzwerke e.V."
+NEWCERT_DURATION = 10*365*24*60*60 # 10 years
 NEWCERT_COMMENT = b'made for you with PyOpenSSL'
 NEWCERT_SIGNDIGEST = "sha1"
 
